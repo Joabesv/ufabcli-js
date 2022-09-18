8e785d80664b70b67540d18e817a2dd9575f5983
Joabesv <joabevarjao123@gmail.com>
2022-09-18 16:14:25 -0300

    feat: create more helpers

--- src/helpers/ruHelpers.js
+++ src/helpers/ruHelpers.js
@@ -10,3 +10,6 @@ export const DISH_IDX_TO_KEY = [
   'salad',
   'dessert',
 ];
+
+export const dayIdxToKey = dx => DAY_IDX_TO_KEY[(dx + 1) / 2 - 1];
+export const dishIdxToKey = mx => DISH_IDX_TO_KEY[(mx + 1) / 2 - 1];
