33296e02470a8d231aa830813aacdfde3681de4f
Joabesv <joabevarjao123@gmail.com>
2022-09-18 15:20:03 -0300

    style: convert require to import and lint files

--- src/commands/ru.js
+++ src/commands/ru.js
@@ -1,37 +1,44 @@
-const request = require('request');
-const cheerio = require('cheerio');
+import request from 'request';
+import { load } from 'cheerio';
 
-const URL_MENU = "http://proap.ufabc.edu.br/nutricao-e-restaurantes-universitarios/cardapio-semanal";
-const DAY_IDX_TO_KEY = ["mon", "tue", "wed", "thu", "fri", "sat"];
-const DISH_IDX_TO_KEY = ["lunch", "diner", "veggie", "garrison", "salad", "dessert"];
+const URL_MENU =
+  'http://proap.ufabc.edu.br/nutricao-e-restaurantes-universitarios/cardapio-semanal';
+const DAY_IDX_TO_KEY = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const DISH_IDX_TO_KEY = [
+  'lunch',
+  'diner',
+  'veggie',
+  'garrison',
+  'salad',
+  'dessert',
+];
 
-module.exports = {
-    thisWeek: () => {
-        return new Promise((resolve) => {
-            let menu = {};
-            request(URL_MENU, (err, res, body) => {
-                if(err) {
-                    // TODO
-                } else {
-                    const $ = cheerio.load(body);
-                    const test = $(".cardapio-semanal > table")[0].children[0].next.children.filter((x) => x.name == "tr");
+export function thisWeek() {
+  return new Promise(resolve => {
+    let menu = {};
+    request(URL_MENU, (err, res, body) => {
+      if (err) {
+      } else {
+        const $ = load(body);
+        const test = $(
+          '.cardapio-semanal > table'
+        )[0].children[0].next.children.filter(x => x.name == 'tr');
 
-                    menu["sun"] = {};
-                    DISH_IDX_TO_KEY.forEach((x) => menu["sun"][x] = "-");
-                    for(let i = 1; i < test.length; i += 2) {
-                        let dx = DAY_IDX_TO_KEY[(i + 1)/2 - 1];
-                        menu[dx] = {};
+        menu['sun'] = {};
+        DISH_IDX_TO_KEY.forEach(x => (menu['sun'][x] = '-'));
+        for (let i = 1; i < test.length; i += 2) {
+          let dx = DAY_IDX_TO_KEY[(i + 1) / 2 - 1];
+          menu[dx] = {};
 
-                        const table = test[i].children[1].children[0].next.children;
+          const table = test[i].children[1].children[0].next.children;
 
-                        for(let j = 1; j < table.length; j += 2) {
-                            let mx = DISH_IDX_TO_KEY[(j + 1)/2 - 1];
-                            menu[dx][mx] = table[j].children[1].data.replace(': ', '');
-                        }
-                    }
-                    resolve(menu);
-                }
-            });
-        })
-    }
+          for (let j = 1; j < table.length; j += 2) {
+            let mx = DISH_IDX_TO_KEY[(j + 1) / 2 - 1];
+            menu[dx][mx] = table[j].children[1].data.replace(': ', '');
+          }
+        }
+        resolve(menu);
+      }
+    });
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,61 +1,53 @@
-const process = require('process');
-const child_process = require('child_process');
+import { ArgumentParser, ArgumentTypeError } from 'argparse';
 
-const http = require('http');
-const cheerio = require('cheerio');
-
-const { ArgumentParser, ArgumentTypeError } = require('argparse');
-
-const ru = require('./commands/ru.js');
+import { thisWeek } from './commands/ru.js';
 
 let parser = new ArgumentParser({
-    version: '0.0.1',
-    addHelp: true,
-    description: 'ufabcli - o app essencial para o rato de computacao'
+  version: '0.0.1',
+  addHelp: true,
+  description: 'ufabcli - o app essencial para o rato de computacao',
 });
 
 let subparsers = parser.addSubparsers({
-    tile: 'subcomands',
-    dest: 'subcommand_name' // wtf
+  tile: 'subcomands',
+  dest: 'subcommand_name', // wtf
 });
 
 let ruParser = subparsers.addParser('ru', { addHelp: true });
-ruParser.addArgument(
-    ["-d", "--day"],
-    {
-        action: "store",
-        type: (x) => { if(x < 0 || x > 5) {
-            throw ArgumentTypeError(`${x} nao e um dia valido!`)
-        } else {
-            return ["mon", "tue", "wed", "thu", "fri", "sat"][x];
-        } },
-        help: 'O dia da semana. 0 = segunda, 5 = sabado'
+ruParser.addArgument(['-d', '--day'], {
+  action: 'store',
+  type: x => {
+    if (x < 0 || x > 5) {
+      throw ArgumentTypeError(`${x} nao e um dia valido!`);
+    } else {
+      return ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'][x];
     }
-);
-ruParser.addArgument(
-    ["-m", "--meal"],
-    {
-        action: "store",
-        type: (x) => {
-            if (["lunch", "diner", "veggie", "garrison", "salad", "dessert"].indexOf(x) == -1) {
-                throw ArgumentTypeError(`${x} nao e uma refeicao valida!`);
-            } else {
-                return x;
-            }
-        },
-        help: 'A refeicao. Pode ser [lunch, diner, veggie, garrison, salad, dessert]'
+  },
+  help: 'O dia da semana. 0 = segunda, 5 = sabado',
+});
+ruParser.addArgument(['-m', '--meal'], {
+  action: 'store',
+  type: x => {
+    if (
+      ['lunch', 'diner', 'veggie', 'garrison', 'salad', 'dessert'].indexOf(x) ==
+      -1
+    ) {
+      throw ArgumentTypeError(`${x} nao e uma refeicao valida!`);
+    } else {
+      return x;
     }
-)
+  },
+  help: 'A refeicao. Pode ser [lunch, diner, veggie, garrison, salad, dessert]',
+});
 
 const main = async () => {
-    const args = parser.parseArgs();
+  const args = parser.parseArgs();
 
-    switch(args.subcommand_name) {
-        case 'ru':
-            const menu = await ru.thisWeek();
-            return menu[args.day][args.meal];
-            break;
-    }
+  switch (args.subcommand_name) {
+    case 'ru':
+      const menu = await thisWeek();
+      return menu[args.day][args.meal];
+  }
 };
 
 main().then(console.log);
