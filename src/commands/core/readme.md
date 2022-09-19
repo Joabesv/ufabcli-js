ac01217711faa00e2e95a20cf3c79e2aa78d9ba6
Joabesv <joabevarjao123@gmail.com>
2022-09-18 22:39:35 -0300

    feat: getting to the foods list, still some loop problems

---
+++ src/commands/core/readme.md
@@ -0,0 +1,2 @@
+# in the future, to break some responsibilities from this Week core func 
+# gonna to break the function in smaller pieces and put it here
--- src/commands/ru.js
+++ src/commands/ru.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 import { load } from 'cheerio';
+import { writeFile } from 'node:fs/promises';
 import {
   DISH_IDX_TO_KEY,
   URL_MENU,
   DAY_IDX_TO_KEY,
+  dayIdxToKey,
+  dishIdxToKey,
 } from '../helpers/ruHelpers.js';
+import { logToFile } from '../utils/log.js';
 
 export async function thisWeek() {
   const { data } = await axios.get(URL_MENU);
@@ -14,29 +18,52 @@ export async function thisWeek() {
 
   const tbody = $(selection)[0].children;
   const tablerow = tbody.filter(tr => tr.name === 'tr');
-
   const menu = {};
   // make sunday always empty
   menu['sun'] = {};
   DISH_IDX_TO_KEY.forEach(sumMenu => (menu['sun'][sumMenu] = null));
 
-  return new Promise(resolve => {
-    request(URL_MENU, (err, res, body) => {
-      if (err) {
-      } else {
-        for (let i = 1; i < test.length; i += 2) {
-          let dx = DAY_IDX_TO_KEY[(i + 1) / 2 - 1];
-          menu[dx] = {};
-
-          const table = test[i].children[1].children[0].next.children;
-
-          for (let j = 1; j < table.length; j += 2) {
-            let mx = DISH_IDX_TO_KEY[(j + 1) / 2 - 1];
-            menu[dx][mx] = table[j].children[1].data.replace(': ', '');
-          }
-        }
-        resolve(menu);
-      }
+  // plan to refactor later but IMO it's better
+  // than nested for's
+  const plates = tablerow
+    .map((plate, idx) => ({
+      plate,
+      idx,
+      days: dayIdxToKey(idx),
+    }))
+    .filter(({ plate, days, idx }) => idx % 2 === 1)
+    .map(({ plate, days, idx }) => ({ plate, days, idx }))
+    .flatMap(async ({ plate, days, idx }) => {
+      const tableData = plate.children[1].children;
+      const foodsList = tableData.filter(list => list.name === 'ul');
+      const foods = foodsList.map(el => ({
+        el,
+        days,
+        idx,
+        mx: dishIdxToKey(idx),
+      }));
+      return foods;
     });
-  });
+
+  // return new Promise(resolve => {
+  //   request(URL_MENU, (err, res, body) => {
+  //     if (err) {
+  //     } else {
+  //       for (let i = 1; i < test.length; i += 2) {
+  //         menu[dx] = {};
+
+  //         const table = test[i].children[1].children[0].next.children;
+
+  //         for (let j = 1; j < table.length; j += 2) {
+  //           menu[dx][mx] = table[j].children[1].data.replace(': ', '');
+  //         }
+  //       }
+  //       resolve(menu);
+  //     }
+  //   });
+  // });
+
+  return plates;
 }
+
+await thisWeek();
